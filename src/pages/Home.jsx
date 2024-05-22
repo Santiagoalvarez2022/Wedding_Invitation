56b61bb2f3e46d1c73fe7402370a7d0010563e4c
santiago alvarez <santiagoalvarez290401@gmail.com>
2024-05-22 14:22:12 -0300

    add border to principal photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ import title_img from "../assets/title.svg"
 export default function Home() { 
   return (
     <div className={style.pageContainer}>
-      <img className={style.img_couple} src={couple_img} alt="" />
-      <img className={style.img_title} src={title_img} alt="" />
+      <img className={style.img_couple} src={couple_img} alt="male y ale" />
+      <img className={style.img_title} src={title_img} alt="nombres" />
 
     </div>
   )
