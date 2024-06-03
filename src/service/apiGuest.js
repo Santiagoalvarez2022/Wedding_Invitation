67814a340bb5a31f0a12f5ee18946e891a23ca9b
santiago alvarez <santiagoalvarez290401@gmail.com>
2024-06-03 16:20:42 -0300

    correcion de peticiones de api

--- src/service/apiGuest.js
+++ src/service/apiGuest.js
@@ -7,13 +7,7 @@ export const get_guest = async(setGuest) =>{
     //setGuest es una funcion que modifica el estado del componente 
     try {
         const {data} = await axios("https://sheet.best/api/sheets/e86d7859-5c14-4bd3-81f4-8fbc09a1b2bd?_raw=1");
-
-
-
-
         setGuest(data)
-
-
     } catch (error) {
         //validar y arrojar error
         console.log("Error en la peticion de lista de invitados ", {error:error.message})
@@ -29,9 +23,8 @@ export const get_guest = async(setGuest) =>{
 export const confirm_guest = async(id) =>{
    
     try {
-        //evaluo si ya la acepto?  
 
-        //modifico lista
+        //ACEPTO LA INVITACION DEL INVITADO CON EL ID PSADO POR PARAMETRO 
         const data = await axios.patch(`https://sheet.best/api/sheets/e86d7859-5c14-4bd3-81f4-8fbc09a1b2bd/${id}`,{Asiste:"SI"})
         return data
 
